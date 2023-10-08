d55224a5724449ed92a73adca81d2ce103015027
Matuyuhi <bird9.yuhi@gmail.com>
2023-10-08 13:06:48 +0900

    Initial Commit

---
+++ metal-ex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  metal-ex
+//
+//  Created by yuhi y on 2023/10/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ metal-ex/metal_exApp.swift
@@ -0,0 +1,17 @@
+//
+//  metal_exApp.swift
+//  metal-ex
+//
+//  Created by yuhi y on 2023/10/08.
+//
+
+import SwiftUI
+
+@main
+struct metal_exApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
